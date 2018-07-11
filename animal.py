edb95d53def9876541504830dd2c992126245eda
rsbabcock <rachael.s.babcock@gmail.com>
2018-07-11 13:27:58 -0500

    added animal class and first test

---
+++ animal.py
@@ -0,0 +1,11 @@
+class Animal():
+
+    def __init__(self):
+        self.size = ""
+        self.food = ""
+        self.legs = 4
+        self.male = False
+        self.female = True
+
+    def say_something(self):
+        return "Speak"
