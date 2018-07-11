8f86d4aaf5b33d74339750ee76a11e7d30fe4c61
pzc <paul.zimmerman.clayton@gmail.com>
2018-07-11 14:10:16 -0500

    test should work now

--- habitat_test.py
+++ habitat_test.py
@@ -1,14 +1,17 @@
 import unittest
 from habitat import Habitat
 
-test_habitat = Habitat()
+try:
+    test_habitat = Habitat()
+except NameError:
+    test_habitat = False
 
 class Habitat_test(unittest.TestCase):
   def test_habitat_is_defined(self):
     """ 
     See if the object has been created
     """
-    self.assertTrue(test_habitat, {})
+    self.assertTrue(test_habitat)
 
   
 if __name__ == '__main__':
