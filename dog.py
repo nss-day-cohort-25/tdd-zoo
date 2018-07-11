0b3a96ad31cbbc98a90598b22531581d62f9b2b3
Matthew Kelly <mkonabike@gmail.com>
2018-07-11 14:33:42 -0500

    dog inherits legs from animal

--- dog.py
+++ dog.py
@@ -1,7 +1,9 @@
 from animal import Animal
+from habitat import Habitat
 
-class Dog (Animal):
+class Dog (Animal, Habitat):
     def __init__(self, name):
         self.name = name
+        Animal.__init__(self)
     def speak():
        return "I am a dog and I live in the forrest"
--- dog_test.py
+++ dog_test.py
@@ -8,9 +8,14 @@ class DogTest(unittest.TestCase):
 
     def test_dog_has_property_name(self):
         self.assertTrue(Dog("Rover").name)
+
     def test_dog_receives_parameter_name(self):
         karma = Dog("Karma")
         self.assertEqual(karma.name, "Karma")
+
+    def test_dog_receives_legs_from_animal(self):
+        karma = Dog("Karma")
+        self.assertEqual(karma.legs, 4)
 
 if __name__ == '__main__':
     unittest.main()
