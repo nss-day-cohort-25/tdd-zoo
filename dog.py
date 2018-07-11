8851cc084ab854048d7cf77075ba6ea0af590460
Matthew Kelly <mkonabike@gmail.com>
2018-07-11 14:19:32 -0500

    added property name and test pass

--- dog.py
+++ dog.py
@@ -1,5 +1,7 @@
 from animal import Animal
 
 class Dog (Animal):
+    def __init__(self, name):
+        self.name = name
     def speak():
        return "I am a dog and I live in the forrest"
--- dog_test.py
+++ dog_test.py
@@ -3,8 +3,14 @@ from dog import Dog
 
 class DogTest(unittest.TestCase):
 
-    def test_dog_speak(self):
+    def test_dog_speak_returns_string(self):
         self.assertEqual(Dog.speak(), "I am a dog and I live in the forrest")
+
+    def test_dog_has_property_name(self):
+        self.assertTrue(Dog("Rover").name)
+    def test_dog_receives_parameter_name(self):
+        karma = Dog("Karma")
+        self.assertEqual(karma.name, "Karma")
 
 if __name__ == '__main__':
     unittest.main()
